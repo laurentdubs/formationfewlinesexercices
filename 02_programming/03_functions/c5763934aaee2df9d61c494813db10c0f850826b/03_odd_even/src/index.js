b95d97ccc444ef9cc5284defbafa0295999d5739
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-13 21:02:38 +0100

    add files 03

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
@@ -1,6 +1,16 @@
-function oddEven() {
+function oddEven(number) {
   // Code the function here.
+  if (Number.isInteger(number)) {
+    if (number % 2 === 0) {
+      return`${number} is an even number.`;
+    }
+    else {
+      return`${number} is an odd number.`;
+    }
+  }
+  else {
+    console.log(`${number} is not a number.`);
+  } 
 }
-
 // Do not remove last lines, it is for tests
 module.exports = oddEven;
