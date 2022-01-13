395d9594310cf037b0e1605442dda5b53a1c61cc
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-13 22:46:50 +0100

    add files 03

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
@@ -1,6 +1,17 @@
-function helloSpartan() {
+function helloSpartan(name = "soldier") {
   // Code the function here.
+  
+  console.log(`Hello ${name}; not very talkative uh?`);
+  console.log(`Hello ${name}: glad to know your name!`);  
+  
+  
 }
+helloSpartan();
+helloSpartan("Francis");
+
+
+
+
 
 // Do not remove last lines, it is for tests
 module.exports = helloSpartan;
