78442124457a50db88705c70ad54ed569463f9b3
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-13 16:29:53 +0100

    add files 03

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
@@ -1,6 +1,6 @@
-function tellFortune() {
-  // Code the function here.
+function tellFortune(job, city ,name, number) {
+  console.log(`You will be ${job} in ${city}, and married to ${name} in ${number} years.`);
 }
-
+tellFortune("webdevelopper","Lille", "Frieda", 2);
 // Do not remove last lines, it is for tests
 module.exports = tellFortune;
