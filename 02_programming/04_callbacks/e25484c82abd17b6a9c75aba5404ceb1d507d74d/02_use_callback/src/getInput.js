ed2f55605c6ca0707040c2fcac2dc533ed94539c
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-14 10:21:33 +0100

    exercices

---
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
@@ -0,0 +1,5 @@
+// A function getInput that takes two parameters, an input (the first one) and a callback function
+
+const getInput = // code the function here
+  // Leave the line below for tests to work
+  (module.exports = getInput);
