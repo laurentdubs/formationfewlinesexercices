aa0241ed56370c9f76ef15b9074cedb74eda2be6
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-14 09:03:17 +0100

    add files 03

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -1,5 +1,11 @@
-function filteredRoles(){
+function filteredRoles([persons], role1 = "Teacher"){
   // Code the function here.
+  if (role1 === "Teacher") {
+    return "Teacher";
+  }
+  else {
+    return "Student";
+  }
 
 }
 
