4cc9447c465fb9cd441604d328da50b4f3e92bed
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-17 15:24:28 +0100

    add files 02

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
@@ -1,5 +1,9 @@
 function theEFilter(sentences) {
-
+  const arrayWithE = sentences.filter((word) => {
+    return word.includes("e") || word.includes("E");
+  });
+  return arrayWithE;
+  
 }
 
 // Do not remove the following line, it is for tests
