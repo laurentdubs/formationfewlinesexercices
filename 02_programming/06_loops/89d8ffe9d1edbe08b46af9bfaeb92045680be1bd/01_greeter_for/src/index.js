560db91fa224d5f0ab937ae012a314dfabf47d17
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-18 10:12:17 +0100

    add ex1 loop

---
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/01_greeter_for/src/index.js
@@ -0,0 +1,9 @@
+function greeter(people) {
+  // Code the function here.
+  for (const name of people) {
+    console.log(`Hello ${name}!`);
+  }
+}
+
+// Do not remove the following lines, it is for tests
+module.exports = greeter;
