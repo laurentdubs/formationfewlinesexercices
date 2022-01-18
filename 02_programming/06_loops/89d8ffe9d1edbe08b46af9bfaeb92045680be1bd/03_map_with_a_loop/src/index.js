07920d7155120367a4feeb2ca7d8ab1748ce5b30
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-18 17:44:46 +0100

    add ex03 loop

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
@@ -1,7 +1,15 @@
-function mapLoop() {
+const numbers = [1, 2, 3, 4];
+function mapLoop(numbers, callback) {
   // Code the function here.
-
+  const resultToReturn = [];
+  for (let i = 0; i < numbers.length; i++) {
+    resultToReturn.push(callback(numbers[i]));
+  }
+  //console.log(resultToReturn);
+  return resultToReturn;
 }
 
+//mapLoop(numbers, (x) => x * 2);
+//yarn startconsole.log(mapLoop(numbers));
 // Do not remove the following lines, it is for tests
 module.exports = mapLoop;
