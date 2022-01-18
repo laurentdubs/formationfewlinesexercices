55a3b6af6493804b45962473c95f54d56246fc4d
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-18 11:16:18 +0100

    add ex2 loop

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/02_for_each_with_a_loop/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/02_for_each_with_a_loop/src/index.js
@@ -1,7 +1,15 @@
-function forEachLoop() {
-  // Code the function here.
+const numbers = [1, 2, 3, 4];
 
+function forEachLoop(numbers, callback) {
+  // Code the function here.) {
+  for (let i = 0; i < numbers.length; i++) {
+    callback(numbers[i]);
+  }
+}
+function log(number) {
+  console.log(`- ${number}`);
 }
 
+forEachLoop(numbers, log);
 // Do not remove the following lines, it is for tests
 module.exports = forEachLoop;
