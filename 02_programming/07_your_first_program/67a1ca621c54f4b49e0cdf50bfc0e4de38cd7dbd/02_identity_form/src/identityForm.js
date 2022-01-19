e2f55277ea3e2fa74da8d44f99300fcb27c9ce82
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-19 12:57:34 +0100

    add ex2 your first program

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
@@ -1,5 +1,22 @@
 const identityForm = (reader) => {
   // Code here
+  reader.question("What is your first name ?\n", (firstname) => {
+    console.log(`Your first name is ${firstname}.`);
+
+    reader.question("What is your last name ?\n", (lastname) => {
+      console.log(`Your first name is ${firstname}, your last name is ${lastname}.`);
+
+      reader.question("What is your age ?\n", (age) => {
+        console.log(`Your name is ${firstname} ${lastname} and you are ${age}.`);
+
+        //reader.question("What is your first name, last name and your age ?\n", (firstname, lastname, age) => {
+        //console.log(`Your name is ${firstname} ${lastname} and you are ${age}`);
+
+        reader.close();
+        //});
+      });
+    });
+  });
 };
 
 // Leave line below for tests to work
