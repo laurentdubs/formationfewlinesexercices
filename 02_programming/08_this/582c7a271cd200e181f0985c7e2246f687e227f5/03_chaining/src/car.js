3d24f7cdee2ddd7de858883feb480cf22f7e0fd7
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-20 18:09:41 +0100

    finish ex3

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/03_chaining/src/car.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/03_chaining/src/car.js
@@ -1,8 +1,31 @@
 // Complete and add needed car properties:
 const car = {
   speed: 0,
+  minutes: 0,
+  distance: 0,
+
   start: function () {
-  
+    this.speed = 0;
+    this.minutes = 0;
+    this.distance = 0;
+    //console.log(`${this.speed}`);
+    return this;
+  },
+  changeSpeed: function (speed) {
+    this.speed = speed;
+    //console.log(`${this.speed}`);
+    return this;
+  },
+  drive: function (minutes) {
+    this.minutes = minutes;
+    //console.log(`${this.minutes}`);
+    this.distance += (this.speed * this.minutes) / 60;
+    //console.log(`${this.distance}`);
+    return this;
+  },
+  showDistance: function () {
+    console.log(`${this.distance} Km`);
+    return this;
   },
 };
 
