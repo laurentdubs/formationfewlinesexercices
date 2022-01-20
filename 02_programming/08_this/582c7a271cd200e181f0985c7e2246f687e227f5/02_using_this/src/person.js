67b0a57b19518b908adb4e1ece42c78f8079be17
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-20 14:29:42 +0100

    finish ex02

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/02_using_this/src/person.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/02_using_this/src/person.js
@@ -6,6 +6,11 @@ const person = {
     return `${this.firstname} ${this.lastname}`;
   },
   // Complete here
+  introduceMyself: function () {
+    return `Hello! I'm ${this.firstname} ${this.lastname} and I'm ${this.age}.`;
+  },
 };
+const introduction = person.introduceMyself();
+console.log(introduction);
 
 module.exports = person;
