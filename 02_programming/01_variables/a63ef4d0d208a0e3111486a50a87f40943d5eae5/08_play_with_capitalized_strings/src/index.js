d82dbd461dba18888371299203892a7cf6efe098
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-25 10:25:50 +0100

    retour sur ex8 clear

--- 02_programming/01_variables/a63ef4d0d208a0e3111486a50a87f40943d5eae5/08_play_with_capitalized_strings/src/index.js
+++ 02_programming/01_variables/a63ef4d0d208a0e3111486a50a87f40943d5eae5/08_play_with_capitalized_strings/src/index.js
@@ -2,4 +2,6 @@ const question = "spartan, what is your profession?!";
 
 // a constant `capitalizedSentence` bound to a string: "Spartan, what is your profession?!"
 // using some of the previous string methods and the constant `question`
-const capitalizedSentence = question.toUpperCase().toLowerCase(question.length-1);
+const capitalizedSentence = question.charAt(0).toUpperCase() + question.slice(1);
+
+console.log(capitalizedSentence);
