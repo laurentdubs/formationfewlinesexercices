dced1eaacb075005316304e4731dd89a394ae15d
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-25 23:14:20 +0100

    retour ex6 clear

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
@@ -1,5 +1,19 @@
-const findPosition = () => {
+const findPosition = (listOfPerson, person, counter) => {
   // Code here
+  if (
+    Array.isArray(listOfPerson) &&
+    typeof listOfPerson[0] === "string" &&
+    typeof person === "string" &&
+    Number.isInteger(counter)
+  ) {
+    if (listOfPerson.includes(person)) {
+      return `${person} is at position #${listOfPerson.indexOf(person) + 1} in this array.`;
+    } else {
+      return `${person} is not present in this array.`;
+    }
+  } else {
+    throw new Error(" A parameter is not of its expected type ");
+  }
 };
 
 // Leave line below for tests to work properly
