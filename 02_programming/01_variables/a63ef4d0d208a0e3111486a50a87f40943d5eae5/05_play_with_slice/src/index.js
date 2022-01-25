adbfa731d7fb41b31857000679e4596acdaa8aaf
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-25 11:06:31 +0100

    retour ex5 clear

--- 02_programming/01_variables/a63ef4d0d208a0e3111486a50a87f40943d5eae5/05_play_with_slice/src/index.js
+++ 02_programming/01_variables/a63ef4d0d208a0e3111486a50a87f40943d5eae5/05_play_with_slice/src/index.js
@@ -2,6 +2,9 @@ const question = "spartan, what is your profession?!";
 
 // a constant `hobbyQuestion` bound to a string: "spartan, what is your hobby?!"
 // using `slice()` and the constant `question`
-const hobbyQuestion = "spartan, what is your hobby?!".slice(question);
+const hobbyQuestion = `${question.slice(0, 21)} hobby?!`;
+console.log(hobbyQuestion);
+
 // a constant `hobbyQuestionLength` bound to a number that is the length of `hobby`
-const hobbyQuestionLength = "hobbyQuestion" .length;
+const hobbyQuestionLength = hobbyQuestion.length;
+console.log(hobbyQuestionLength);
