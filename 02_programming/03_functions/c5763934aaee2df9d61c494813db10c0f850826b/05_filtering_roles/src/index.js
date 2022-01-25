cb2ba8b9fe7ba463c4be8a04c3763707c443976e
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-25 14:10:23 +0100

    retour sur ex5 clear

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -1,12 +1,20 @@
-function filteredRoles([persons], role1 = "Teacher"){
+function filteredRoles(persons, roles = "Teacher") {
   // Code the function here.
-  if (role1 === "Teacher") {
-    return "Teacher";
-  }
-  else {
-    return "Student";
-  }
+  const mySortedArray = [];
 
+  if (persons[0].role === roles) {
+    mySortedArray.push({ name: persons[0].name, role: roles });
+  }
+  if (persons[1].role === roles) {
+    mySortedArray.push({ name: persons[1].name, role: roles });
+  }
+  if (persons[2].role === roles) {
+    mySortedArray.push({ name: persons[2].name, role: roles });
+  }
+  if (persons[3].role === roles) {
+    mySortedArray.push({ name: persons[3].name, role: roles });
+  }
+  return mySortedArray;
 }
 
 // Do not remove lines below, it is for tests
