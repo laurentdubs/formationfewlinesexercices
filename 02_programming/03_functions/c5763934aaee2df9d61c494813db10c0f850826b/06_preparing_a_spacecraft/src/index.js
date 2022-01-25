8e549a12b3074754dd9dc914240ab799e8d3a994
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-25 15:57:06 +0100

    retour ex7 clear

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
@@ -1,9 +1,24 @@
-function checkFuelLevel() {
+function checkFuelLevel(fuelLevel) {
   // Code here
+
+  if (fuelLevel > 20) {
+    return "Fuel level: green";
+  } else if (fuelLevel > 10) {
+    return "Fuel level: yellow";
+  } else {
+    return "Fuel level: red";
+  }
 }
 
-function checkCargoHold(){
+function checkCargoHold(cargoLoad) {
   // Code here
+  if (cargoLoad.length === 4) {
+    return "Full";
+  } else if (cargoLoad.length < 4) {
+    return `Spaces available: ${4 - cargoLoad.length}`;
+  } else {
+    return `Over capacity by ${cargoLoad.length - 4} items.`;
+  }
 }
 
 // Do not remove last lines, it is for tests
--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
@@ -1,4 +1,23 @@
 // Code here
+const stealthyFuelLevelCheck = (fuelLevel) => {
+  if (fuelLevel > 20) {
+    return "Fuel level: green";
+  } else if (fuelLevel > 10) {
+    return "Fuel level: yellow";
+  } else {
+    return "Fuel level: red";
+  }
+};
+
+const stealthyCargoHoldCheck = (cargoLoad) => {
+  if (cargoLoad.length === 4) {
+    return "Full";
+  } else if (cargoLoad.length < 4) {
+    return `Spaces available: ${4 - cargoLoad.length}`;
+  } else {
+    return `Over capacity by ${cargoLoad.length - 4} items.`;
+  }
+};
 
 // Do not remove last lines, it is for tests
 
