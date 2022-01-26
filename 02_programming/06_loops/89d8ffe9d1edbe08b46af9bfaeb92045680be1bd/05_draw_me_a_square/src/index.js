5e9cda27f77717b62a4aad1a8ddc28c2d2c3d8a8
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-26 11:07:32 +0100

    retour ex5 clear

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/05_draw_me_a_square/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/05_draw_me_a_square/src/index.js
@@ -1,6 +1,16 @@
-function square() {
+function square(sizeOfSquare) {
   // Code here
+  let size = "";
+  //console.log(sizeOfSquare.length);
+
+  for (let i = 0; i < sizeOfSquare; i++) {
+    size = size + "*";
+  }
+  for (let i = 0; i < sizeOfSquare; i++) {
+    console.log(size);
+  }
 }
+square(5);
 
 // Do not remove the following line, it is for tests
 module.exports = square;
