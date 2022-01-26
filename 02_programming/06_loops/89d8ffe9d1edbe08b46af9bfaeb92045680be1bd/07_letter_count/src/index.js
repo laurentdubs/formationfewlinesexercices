deb542fb7ce8c30d7f24410cf4961c8b84112b25
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-26 12:22:45 +0100

    retour ex7 clear

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/07_letter_count/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/07_letter_count/src/index.js
@@ -1,6 +1,16 @@
-function letterCount() {
+function letterCount(word, letter) {
   // Code here
-}
+  let count = 0;
 
+  for (let i = 0; i < word.length; i++) {
+    if (word.charAt(i) === letter) {
+      count++;
+    }
+  }
+  return count;
+}
+letterCount("gattaca", "a");
+console.log(letterCount("gattaca", "a"));
+console.log(letterCount("azerty", "b"));
 // Do not remove the following line, it is for tests
 module.exports = letterCount;
