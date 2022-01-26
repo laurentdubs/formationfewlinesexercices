edd63411b8cdae1417d58973774eb392c18abd6e
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-26 11:41:43 +0100

    retour ex6 clear

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/06_draw_me_a_triangle/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/06_draw_me_a_triangle/src/index.js
@@ -1,6 +1,12 @@
-function triangle() {
+function triangle(heightOfTriangle) {
   // Code here
-}
+  let height = "";
 
+  for (let i = 0; i < heightOfTriangle; i++) {
+    height += "*";
+    console.log(height);
+  }
+}
+triangle(4);
 // Do not remove the following line, it is for tests
 module.exports = triangle;
