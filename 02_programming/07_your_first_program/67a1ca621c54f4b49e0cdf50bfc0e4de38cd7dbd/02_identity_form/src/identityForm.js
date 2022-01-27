45e70e9a1fe47583d26d92447c636e13ebf7243f
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-27 10:53:30 +0100

    retour ex2 clear

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
@@ -1,14 +1,11 @@
 const identityForm = (reader) => {
   // Code here
-  reader.question("What is your first name ?\n", (firstname) => {
-    console.log(`Your first name is ${firstname}.`);
-
-    reader.question("What is your last name ?\n", (lastname) => {
-      console.log(`Your first name is ${firstname}, your last name is ${lastname}.`);
-
+  reader.question("What is your first name ?\n", (firstName) => {
+    //console.log(`Your first name is ${firstName}.`);
+    reader.question("What is your last name ?\n", (lastName) => {
+      //console.log(`Your first name is ${firstName}, your lastname is ${lastName}.`);
       reader.question("What is your age ?\n", (age) => {
-        console.log(`Your name is ${firstname} ${lastname} and you are ${age}.`);
-
+        console.log(`Your name is ${firstName} ${lastName} and you are ${age}.`);
         reader.close();
       });
     });
