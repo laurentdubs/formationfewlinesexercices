b8af11a78440a2760f2d760ca4ce0665670b0dbc
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-27 09:40:54 +0100

    retour ex8

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/08_consecutive_count/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/08_consecutive_count/src/index.js
@@ -1,6 +1,35 @@
-function consecutiveCount() {
+function consecutiveCount(word, letter) {
   // Code here
+  let countLetter = 0;
+  let countStreakLetter = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    if (word.charAt(i) === letter) {
+      countLetter++;
+    } else {
+      if (countLetter > countStreakLetter) {
+        countStreakLetter = countLetter;
+        countLetter = 0;
+      } else {
+        countLetter = 0;
+      }
+    }
+
+    if (countLetter > countStreakLetter) {
+      countStreakLetter = countLetter;
+    }
+    if (countStreakLetter === 0 && countLetter === 1) {
+      return 1;
+    } else if (countStreakLetter === 0) {
+      return 0;
+    } else {
+      return countStreakLetter;
+    }
+  }
 }
+consecutiveCount("aaabcaaba", "a");
+console.log(consecutiveCount("aaabcaaba", "a"));
+consecutiveCount("utuutuuuuutuu", "u");
 
 // Do not remove the following line, it is for tests
 module.exports = consecutiveCount;
