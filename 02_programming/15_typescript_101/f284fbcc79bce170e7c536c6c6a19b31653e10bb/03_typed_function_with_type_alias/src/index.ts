4c36ffdb79fb46a22ad8e2ecbbcc1ed2e291ce53
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-01-31 15:14:21 +0100

    ex3 clear

--- 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
+++ 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
@@ -1,6 +1,21 @@
-function handleTuple() {
-  // Code the function here.
+type MyTuple = [string, number];
+//const plop: MyTuple = ["hello", 42]; 
+function handleTuple(tuple: MyTuple): void {
+// Code the function here.
+  if (typeof tuple[0] === "string") {
+    console.log(`${tuple[0]} is a string`);
+    
+  }
+  if (typeof tuple[1] === "number") {
+    console.log(`${tuple[1]} is a number`);
+    
+  }
 }
+
+
+handleTuple(["foo", 42]);
+//console.log(handleTuple(["foo", 42]));
+
 
 // Leave the line below for tests to work properly.
 export { handleTuple };
