4091ccd549cfd1264a0328692e893364e2ba34bb
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-01 15:12:11 +0100

    ex2 clear

--- 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
@@ -1,4 +1,44 @@
 // Code the class here.
+class Tree {
+    age: number;
+    height: number;
+    alive: boolean = true;
+
+    constructor(age: number) {
+        this.age = age;
+        
+        if (this.age === 0){
+            this.height = 0;
+            //console.log(this.heigth)
+        }
+        if (this.age >= 1 && this.age <= 9) {
+            this.height = this.age * 25;
+            //console.log(this.heigth);
+        }
+        if (this.age >= 10 && this.age <= 20) {
+            this.height = (this.age - 9) * 10 + 225;
+            //console.log(this.heigth);
+            
+        }
+        if (this.age > 20){
+            this.height = 335;
+            //console.log(this.heigth);
+        }
+    
+        
+       
+    }
+
+    seed():void {
+    this.age = 0;
+    this.height = 0;
+    this.alive = true;
+
+    }
+}
+const youngOrangeTree = new Tree(10);
+
+console.log({youngOrangeTree})
 
 // Leave the line below for tests to work properly.
 export { Tree };
