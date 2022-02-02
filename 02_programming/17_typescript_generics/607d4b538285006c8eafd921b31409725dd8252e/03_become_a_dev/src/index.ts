eb194f84ddccee87ed7ffe1e6fc257fce848a301
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-02 17:07:57 +0100

    ex3 clear

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/03_become_a_dev/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/03_become_a_dev/src/index.ts
@@ -1,4 +1,27 @@
+//import { stringify } from "querystring";
+//import { DEFAULT_MAX_VERSION } from "tls";
+//import { Z_HUFFMAN_ONLY } from "zlib";
 
+type Human = {
+    name: string;
+    age: number;
+    knowsDev: boolean;
+}
+type Dev = Human & {knownLanguage: string};
+
+function becomeADev(object: Human): Dev {
+    const result: Dev = {
+        ...object,
+        knowsDev: true,
+        knownLanguage: "JavaScript",
+    }
+    
+        return result;
+  
+}
+
+//becomeADev(result);
+//console.log(becomeADev());
 
 // Leave the line below for tests to work properly.
 export { becomeADev };
