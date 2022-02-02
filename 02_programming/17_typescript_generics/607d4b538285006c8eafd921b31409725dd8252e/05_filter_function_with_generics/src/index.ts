b921851a80094fdbe1d7f24ec3b4fc784244baa9
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-02 17:33:50 +0100

    ex5 clear

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/05_filter_function_with_generics/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/05_filter_function_with_generics/src/index.ts
@@ -1,4 +1,5 @@
-function filterFor(array, predicate) {
+function filterFor<ArrayElementType>(array: ArrayElementType[], predicate: (element: ArrayElementType) => 
+  boolean): ArrayElementType[]{
   const resultArray = [];
 
   for (const element of array) {
