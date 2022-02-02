e9c52a474b3e87f052c5d316613f799961f05a98
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-02 15:20:05 +0100

    ex2 clear

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
@@ -1,3 +1,19 @@
+type Product = {
+    productName: string;
+    quantity: number;
+}
+
+function productTransformer(object: Product): [string,string | number][] {
+    return Object.entries(object);
+}
+
+
+
+
+
+console.log(productTransformer({productName:"plop", quantity: 2}));
+
+
 
 // Leave the line below for tests to work properly.
 export { productTransformer };
