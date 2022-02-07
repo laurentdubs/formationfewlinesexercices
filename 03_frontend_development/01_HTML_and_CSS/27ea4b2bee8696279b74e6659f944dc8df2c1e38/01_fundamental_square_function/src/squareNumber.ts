42ea77599f4f3856f931e9979e387de4247a5c43
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-07 10:08:39 +0100

    ex1 clear

---
+++ 03_frontend_development/01_HTML_and_CSS/27ea4b2bee8696279b74e6659f944dc8df2c1e38/01_fundamental_square_function/src/squareNumber.ts
@@ -0,0 +1,6 @@
+export function squareNumber(number: number): number {
+    return number * number;
+
+}
+squareNumber(7)
+console.log(squareNumber(7));
---
+++ 03_frontend_development/01_HTML_and_CSS/27ea4b2bee8696279b74e6659f944dc8df2c1e38/02_fundamental_square_context/src/context.ts
@@ -0,0 +1,5 @@
+import {squareNumber} from "./squareNumber"
+
+export function context(arrayOfNumbers: number[]): number[] {
+  // Code the function here
+}
