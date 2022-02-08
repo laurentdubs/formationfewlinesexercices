4a77a43493c155ff3fa60be174ca30749b698a57
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-08 11:42:21 +0100

    ex2 warmup clear

--- 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/02_warmup_sorting_machine/src/index.ts
+++ 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/02_warmup_sorting_machine/src/index.ts
@@ -1,1 +1,16 @@
 // Code here
+const numArray = [10000, 104, 99, 2000, 3000, 1500];
+
+function sortingMachine(numArray: number[], type :boolean): number[] {
+    if(type === true) {
+        return numArray.sort((a,b) => a - b);
+    }
+    else{
+        return numArray.sort((a,b) => b - a);
+    }
+}
+sortingMachine(numArray, true);
+console.log(numArray);
+
+
+export{sortingMachine};
