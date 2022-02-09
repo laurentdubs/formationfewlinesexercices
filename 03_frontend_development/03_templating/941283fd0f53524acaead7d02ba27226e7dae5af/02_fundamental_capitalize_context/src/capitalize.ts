efbdd32b074eccf4699d3fffa24dd494aa801f86
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-09 10:23:26 +0100

    ex1 warmup clear

---
+++ 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/capitalize.ts
@@ -0,0 +1,5 @@
+// Copy your function `capitalize` from the first exercise
+
+export function capitalize(word: string): string {
+  // Code your function here
+}
---
+++ 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/context.ts
@@ -0,0 +1,5 @@
+import { capitalize } from "./capitalize"
+
+export function context(sentence: string): string {
+  // Code here and use the function `capitalize` from exercise one
+}
