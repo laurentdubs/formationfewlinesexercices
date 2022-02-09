1580474b3cefd86c3033e55e27d0c5ab368df749
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-09 10:54:26 +0100

    ex2 warmup clear

--- 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/capitalize.ts
+++ 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/capitalize.ts
@@ -2,4 +2,5 @@
 
 export function capitalize(word: string): string {
   // Code your function here
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
--- 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/context.ts
+++ 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/context.ts
@@ -2,4 +2,9 @@ import { capitalize } from "./capitalize"
 
 export function context(sentence: string): string {
   // Code here and use the function `capitalize` from exercise one
+  return sentence
+  .toLowerCase()
+  .split(" ")
+  .map(word => capitalize(word))
+  .join(' ');
 }
