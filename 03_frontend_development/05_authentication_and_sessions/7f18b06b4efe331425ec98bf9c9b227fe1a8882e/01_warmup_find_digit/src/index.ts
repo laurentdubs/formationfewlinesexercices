9c22d004b38d0dd6280307464d146b9cd4734cbc
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-11 11:50:44 +0100

    ex1 clear

---
+++ 03_frontend_development/05_authentication_and_sessions/7f18b06b4efe331425ec98bf9c9b227fe1a8882e/01_warmup_find_digit/src/index.ts
@@ -0,0 +1,15 @@
+// Code here
+const anArrayOfNumbers: number[] = [1, 92, 824];
+
+function findDigit(anArrayOfNumbers:number[], digit:number): number[] {
+   //const result: number[] = [];
+   return anArrayOfNumbers.filter((number) => 
+   number.toString().includes(digit.toString()));
+}
+
+findDigit(anArrayOfNumbers, 2);
+console.log(findDigit(anArrayOfNumbers, 2));
+
+//findDigit(anArrayOfNumbers, 3);
+
+export {findDigit};
