00fe55dee6ef8719dc5539a6ccc1b43f1db5136a
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-14 12:12:23 +0100

    ex2 clear

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/02_warmup_loto_checker/src/check.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/02_warmup_loto_checker/src/check.ts
@@ -3,4 +3,23 @@ import { Draw } from "./types";
 
 export function checkIfIWon(ticket: Draw): void {
   // Code the function here
+  const numbersOfTicket = lottery_draw();
+
+  let result = 0;
+
+  for (let i = 0; i < numbersOfTicket.length; i++) {
+    if (ticket[i] === numbersOfTicket[i]) {
+    result ++;
+      
+    }
+  }
+  if (result === 3) {
+    console.log("You won the lottery!");
+    
+  }else {
+    console.log("You lost...");
+    
+  }
 }
+
+  checkIfIWon([1, 2, 3]);
--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/02_warmup_loto_checker/src/lottery_draw.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/02_warmup_loto_checker/src/lottery_draw.ts
@@ -1,5 +1,18 @@
 import { Draw } from "./types";
 
-export function lottery_draw(): Draw {
+function getRamdomNumbers(min: number, max: number) {
 
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+ 
 }
+
+export function lottery_draw(): Draw {
+    const numbersOfArray = [];
+    for (let i = 0; i < 3; i++) {
+        numbersOfArray.push(getRamdomNumbers(0, 3));
+        
+    }
+    return numbersOfArray;
+}
+
+console.log(lottery_draw());
