16a20b2c623faa5db6845dcfa78cd7112f8f8ada
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-14 16:02:00 +0100

    ex2 clear

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getCategories.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getCategories.ts
@@ -1,7 +1,17 @@
 import request from "@fewlines-education/request";
 
-function getCategories() {
+function getCategories(): void {
   // code the function here
+  request("https://api.chucknorris.io/jokes/categories", (error, body) => {
+    if (error) {
+      console.error(error);
+      
+    }else {
+      const data = JSON.parse(body);
+      console.log(data);
+      
+    }
+  })
 }
 
 // leave line below for tests to work properly
--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getChuckNorrisJoke.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getChuckNorrisJoke.ts
@@ -1,8 +1,20 @@
-import request from "@fewlines-education/request";
+import request, { Request } from "@fewlines-education/request";
 
-function getChuckNorrisJoke() {
+function getChuckNorrisJoke(selectedCategory: string ): void {
   // code the function here
-}
+  request("https://api.chucknorris.io/jokes/random?category=" + selectedCategory, (error, body) => {
+   if (error) {  
+  console.error(error);
+    }
+    else {
+    const data = JSON.parse(body);
+    console.log(data);
+    
+    }
 
+    
+  });
+  
+}
 // leave line below for tests to work properly
 export { getChuckNorrisJoke };
