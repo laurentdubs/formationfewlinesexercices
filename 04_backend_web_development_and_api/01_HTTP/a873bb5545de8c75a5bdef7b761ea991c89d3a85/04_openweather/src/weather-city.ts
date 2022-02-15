5b8ed78cefc8e98ff69f002dc8986d118434fb73
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-15 09:23:03 +0100

    ex4 almost done

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-city.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-city.ts
@@ -1,10 +1,19 @@
 import request from "@fewlines-education/request";
 import "dotenv/config";
 
-function weatherByCity(city) {
+function weatherByCity(city: string): void {
   // code the function here
+  request(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`,
+  (error, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const data = JSON.parse(body);
+      console.log(`${data.main.temp} °C`);
+    }
+  }
+)
 }
-
 // leave lines below for tests to work properly
 export { weatherByCity }
 
--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-forecast.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-forecast.ts
@@ -1,13 +1,32 @@
 import request from "@fewlines-education/request";
 import "dotenv/config";
 
-function weatherByZipcode(zipcode, countryCode) {
+function weatherByZipcode(zipcode: string, countryCode: string):void {
   // code the function here
+  request(`http://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},${countryCode}&units=metric&cnt=16&appid=${process.env.OPENWEATHER_API_KEY}`,(error, body) => {
+     if(error){
+       console.error(error);
+    }else{
+     const data = JSON.parse(body);
+      console.log(`Weather for ${data.city.name}`);
+     
+    }
+   })
 }
 
-function weatherByLatitudeAndLongitude(latitude, longitude) {
+function weatherByLatitudeAndLongitude(latitude: number, longitude: number):void {
   // code the function here
+  request(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&cnt=16&appid=${process.env.OPENWEATHER_API_KEY}`,(error, body) => {
+    if(error){
+      console.error(error);
+   }else{
+     const data = JSON.parse(body);
+     console.log(`Weather for ${data.city.name}`);
+     
+   }
+  })
 }
+
 
 // leave lines below for tests to work properly
 export {
