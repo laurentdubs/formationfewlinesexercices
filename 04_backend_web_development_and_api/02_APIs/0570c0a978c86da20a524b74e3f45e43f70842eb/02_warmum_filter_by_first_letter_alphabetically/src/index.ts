9e5ce6d179bc80d22afa2ee24b41b9ef0b47cda5
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-15 11:37:51 +0100

    ex2 clear

--- 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
+++ 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
@@ -1,1 +1,28 @@
 // code here
+import { Customer } from "../data/customers";
+
+function filterByFirstLetter(customers: Customer[], letter: string): Customer[] {
+  // Code here
+ const arrayFiltered = customers.filter((customers) => letter === customers.last_name[0]) 
+  
+ return arrayFiltered;
+}
+
+export function filterByFirstLetterOrdered(customers: Customer[], letter:string): Customer[] {
+    const arrayFilteredInAlphabeticalOrder = filterByFirstLetter(customers, letter).sort((a, b) => {
+    if (a.last_name > b.last_name === true) {
+       return 1;
+     }
+    else if (b.last_name > b.last_name === false) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
+    })
+
+
+    console.log(arrayFilteredInAlphabeticalOrder);
+    
+    return arrayFilteredInAlphabeticalOrder;
+}
