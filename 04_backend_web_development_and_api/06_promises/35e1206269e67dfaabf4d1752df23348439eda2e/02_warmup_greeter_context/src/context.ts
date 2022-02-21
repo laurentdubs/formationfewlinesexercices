279690e4b29fd5a0549a9b84262f17fb8468d0ef
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-21 10:59:59 +0100

    ex2 clear

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/02_warmup_greeter_context/src/context.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/02_warmup_greeter_context/src/context.ts
@@ -1,6 +1,10 @@
+import { greetEveryone } from "./greetEveryone";
+
 type ParamRest = string[];
 type CallbackType= (...restParam: ParamRest) => void;
 
 export function contextFunction(callbackFunction : CallbackType, parameters: ParamRest): void {
   // Code your function here
+ parameters.forEach((el) => console.log(`We now greet: ${el}`))
+ callbackFunction(...parameters);
 }
--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/02_warmup_greeter_context/src/greetEveryone.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/02_warmup_greeter_context/src/greetEveryone.ts
@@ -1,1 +1,6 @@
 // Copy your function greetEveryone here
+export function greetEveryone(...ParamRest: string[]): void {
+    // Code your function here
+    console.log(`Welcome to ${ParamRest}`);
+    
+}
