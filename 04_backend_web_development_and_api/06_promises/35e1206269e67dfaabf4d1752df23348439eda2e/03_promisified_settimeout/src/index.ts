6ce0b1f718f3db53c3372fe7e8fd8e466e7c93a2
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-21 12:16:18 +0100

    ex3 clear

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/03_promisified_settimeout/src/index.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/03_promisified_settimeout/src/index.ts
@@ -1,5 +1,13 @@
 function waitFor(time: number): Promise<void> {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(console.log("this will be printed after 2 seconds"));
+    }, time);
+  });
 }
+
+waitFor(2000).then(() => {
+  console.log("This will be printed after 2 seconds");
+});
 
 export { waitFor };
