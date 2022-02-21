a026ebb663eda161896b60a1ea6d7da23c77e391
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-21 15:02:49 +0100

    ex4 clear

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/04_chuck_norris_api_with_promises/src/chuck.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/04_chuck_norris_api_with_promises/src/chuck.ts
@@ -1,9 +1,15 @@
-function getCategories() {
+import fetch from "node-fetch";
+
+function getCategories(): Promise<string[]> {
   // Your code goes here
+  return fetch("https://api.chucknorris.io/jokes/categories").then((response) => response.json());
 }
 
-function getJoke() {
+function getJoke(categories: string): Promise<string> {
   // Your code goes here
+  return fetch(`https://api.chucknorris.io/jokes/random?category=${categories}`)
+    .then((response) => response.json())
+    .then((joke) => joke.value);
 }
 
 // Leave the line below for tests to work properly
