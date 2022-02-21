6f6995fe1aa81db54d77b0132c2f1783b4ca8282
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-21 15:48:58 +0100

    ex5 clear

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/05_head_or_tail/src/head-or-tail.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/05_head_or_tail/src/head-or-tail.ts
@@ -1,7 +1,13 @@
 import { getRandomFace } from "./random-face";
 
-function headOrTail() {
-
+function headOrTail(head: string, tail: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    if (getRandomFace() === head || tail) {
+      resolve("Well done.");
+    } else {
+      reject("Nope.");
+    }
+  });
 }
 
 export { headOrTail };
