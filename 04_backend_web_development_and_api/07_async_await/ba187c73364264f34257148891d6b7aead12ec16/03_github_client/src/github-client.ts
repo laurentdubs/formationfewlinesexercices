4aef71c532f2fdc4e3824631e1d0d73422b708fa
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-23 00:36:14 +0100

    ex4 clear

--- 04_backend_web_development_and_api/07_async_await/ba187c73364264f34257148891d6b7aead12ec16/03_github_client/src/github-client.ts
+++ 04_backend_web_development_and_api/07_async_await/ba187c73364264f34257148891d6b7aead12ec16/03_github_client/src/github-client.ts
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+import fetch, { FetchError, Response } from "node-fetch";
 
 export type Repo = {
   url: string;
@@ -12,24 +12,55 @@ export type GitHub = {
 }
 
 
-export const getReposUrl = () => {
+export const getReposUrl = async(nickname: string): Promise<string> => {
   // Code here
+  const response = await fetch(`https://api.github.com/users/${nickname}`);
+  const user = await response.json();
+  
+    if (user.message === "Not Found") {
+      throw new Error("The user doesn't exist");
+    }
+  
+   return user.repos_url;     
 }
 
-export const getRepos = () => {
+export const getRepos = async(url: string): Promise<string[]>  => {
   // Code here
+  const response = await fetch(url);
+  const repositoriesList = await response.json();
+
+  console.log(31, repositoriesList);
+  return repositoriesList;
+  
 }
 
-export const printRepos = () => {
+export const printRepos = (repos: Repo[]): Repo[] =>  {
   // Code here
+  for (let i = 0; i < repos.length; i++) {
+    console.log(`${i+ 1} - ${repos[i].name}`);
+  }
+
+  return repos;
 }
 
 
-export const printRepository = () => {
+export const printRepository = (repository: Repo): void  => {
   // Code here
+
+  console.log(repository.name);
+  console.log(repository.description);
+  console.log(repository.subscribers_count);
+  console.log(repository.stargazers_count);
+  console.log(repository.language);
+  console.log(repository.url);
+  
 }
 
-export const getProjectInformations = () => {
+export const getProjectInformations = async(url: string): Promise<string> => {
   // Code here
+  const response = await fetch(url);
+  const dataRepository = await response.json();
+
+  return dataRepository;
 }
 
