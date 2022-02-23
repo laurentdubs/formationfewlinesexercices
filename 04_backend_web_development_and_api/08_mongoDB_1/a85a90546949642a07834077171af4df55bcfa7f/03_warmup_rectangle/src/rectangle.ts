509b5ce3b1b19284229d59647773558c008baea4
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-02-23 15:23:27 +0100

    ex4 clear

--- 04_backend_web_development_and_api/08_mongoDB_1/a85a90546949642a07834077171af4df55bcfa7f/03_warmup_rectangle/src/rectangle.ts
+++ 04_backend_web_development_and_api/08_mongoDB_1/a85a90546949642a07834077171af4df55bcfa7f/03_warmup_rectangle/src/rectangle.ts
@@ -1,2 +1,11 @@
 export function rectangle(width: number, height: number): string {
+let area = "";
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            area += "*";
+        }
+            area += "\n"
+    } 
+   return area;
 }
+
