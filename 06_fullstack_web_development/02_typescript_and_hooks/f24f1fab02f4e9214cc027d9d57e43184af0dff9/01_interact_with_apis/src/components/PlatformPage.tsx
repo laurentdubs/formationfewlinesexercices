229c22b1becb55195e4ab4364028151338aee52e
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-03-16 15:10:39 +0100

    ex no finish

--- 06_fullstack_web_development/02_typescript_and_hooks/f24f1fab02f4e9214cc027d9d57e43184af0dff9/01_interact_with_apis/src/components/PlatformPage.tsx
+++ 06_fullstack_web_development/02_typescript_and_hooks/f24f1fab02f4e9214cc027d9d57e43184af0dff9/01_interact_with_apis/src/components/PlatformPage.tsx
@@ -1,7 +1,53 @@
 import React from "react";
 
 const PlatformPage = (): JSX.Element => {
-  return <>Platforms</>;
+  type Platform = {
+    name: string;
+    id: string;
+  };
+
+  type Game = {
+    name: string;
+    id: string;
+  };
+  const [platform, setPlatform] = React.useState([]);
+  const [game, setGames] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("http://videogame-api.fly.dev/platforms")
+      .then((response) => response.json())
+      .then((element) => {
+        const platformSelected = element.platforms.map((platform: Platform) => {
+          return { id: platform.id, name: platform.name };
+        });
+        setPlatform(platformSelected);
+      });
+  }, []);
+
+  React.useEffect(() => {
+    fetch("http://videogame-api.fly.dev/games")
+      .then((response) => response.json())
+      .then((element) => {
+        const gameSelected = element.games.map((game: Game) => {
+          return { id: game.id, name: game.name };
+        });
+        setGames(gameSelected);
+      });
+  }, []);
+
+  return (
+    <div>
+      <select>
+        {platform.map((platform: Platform) => {
+          return (
+            <option value={platform.id} key={platform.id}>
+              {platform.name}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
 };
 
 export default PlatformPage;
