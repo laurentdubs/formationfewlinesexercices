b2a57bdd243c7495c746c3717f5d65d2c63aa202
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-03-16 09:55:06 +0100

    ex3 clear

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
@@ -1,5 +1,46 @@
 import React from "react";
 
-const Form = () => "Code here";
+const Form = () => {
+  const [firstName, setFirstName] = React.useState("");
+  const [firstNameError, setFirstNameError] = React.useState(null);
+  const validateFirstName = (firstName) => {
+    if (firstName.includes(" ")) {
+      setFirstNameError("No spaces allowed");
+      setFirstName(firstName);
+    } else {
+      setFirstNameError(null);
+      setFirstName(firstName);
+    }
+  };
+
+  return (
+    <form
+      className="w-100"
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
+      <div className="form-group">
+        <label htmlFor="first-name">First Name</label>
+        <input
+          type="text"
+          className="form-control"
+          id="first-name"
+          aria-describedby="firstNameHelp"
+          value={firstName}
+          onChange={(event) => validateFirstName(event.target.value)}
+        />
+        {firstNameError ? (
+          <small id="firstNameHelp" className="form-text text-muted">
+            {firstNameError}
+          </small>
+        ) : null}
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
 
 export default Form;
