bf07bf4abc8b6579bf3b41441e53978c1c22b466
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-03-17 09:15:48 +0100

    clean git

--- 06_fullstack_web_development/02_typescript_and_hooks/f24f1fab02f4e9214cc027d9d57e43184af0dff9/02_react_timers/src/components/Timer.tsx
+++ 06_fullstack_web_development/02_typescript_and_hooks/f24f1fab02f4e9214cc027d9d57e43184af0dff9/02_react_timers/src/components/Timer.tsx
@@ -6,5 +6,20 @@ type TimerProps = {
 };
 
 export const Timer: React.FC<TimerProps> = ({ timerId, removeTimer }) => {
-  return <p>TIMER</p>;
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setCount(count + 1);
+    }, 1000);
+  }, [count]);
+
+  return (
+    <li className="list-group-item d-flex align-items-center  justify-content-between" id={timerId}>
+      <p className="text-center">{count} seconds</p>
+      <div className="btn btn-danger" onClick={removeTimer}>
+        Delete
+      </div>
+    </li>
+  );
 };
--- 06_fullstack_web_development/02_typescript_and_hooks/f24f1fab02f4e9214cc027d9d57e43184af0dff9/02_react_timers/src/components/TimersList.tsx
+++ 06_fullstack_web_development/02_typescript_and_hooks/f24f1fab02f4e9214cc027d9d57e43184af0dff9/02_react_timers/src/components/TimersList.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { Timer } from "./Timer";
+import { v4 as uuidv4 } from "uuid";
 
 const TimersList: React.FC = () => {
-  return <p>TIMERS LIST</p>;
+  const [timer, setTimer] = React.useState<string[]>([]);
+
+  function addTime() {
+    console.log(timer);
+    const idTimer = uuidv4();
+    return setTimer([...timer, idTimer]);
+  }
+
+  function removeTime(param: string) {
+    return setTimer(timer.splice(timer.indexOf(param, 1)));
+  }
+
+  return (
+    <ul className="list-group timers-list">
+      <li className="list-group-item text-center">
+        <button className="btn btn-primary" onClick={addTime}>
+          Add a timer
+        </button>
+      </li>
+      {timer.map((item, index) => {
+        return <Timer key={index} timerId={item} removeTimer={removeTime} />;
+      })}
+    </ul>
+  );
 };
 
 export default TimersList;
